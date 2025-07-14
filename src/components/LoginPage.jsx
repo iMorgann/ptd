7231527bede1442611655fc5d73f0a44d7f8851b
imorgann <imanuelnwachukwu17@gmail.com>
2025-07-14 06:38:21 -0700

    yeptd

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
     setLoginStatus('');
 
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://ptdbackend.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
